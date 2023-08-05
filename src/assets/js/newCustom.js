3b403fe2a4f6af9e8c9233952dc78777e33ace8c
SanjarKeySkills <forest2004@mail.ru>
2023-08-06 01:29:38 +0600

    add lorem 39

--- src/assets/js/newCustom.js
+++ src/assets/js/newCustom.js
@@ -2,7 +2,19 @@ import React, { Component } from "react";
 
 class newCustom extends Component {
   render() {
-    return <div>twist</div>;
+    return (
+      <div>
+        <div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque,
+            corporis molestias impedit totam dignissimos itaque quia eum
+            corrupti mollitia possimus deserunt temporibus minus soluta, quis
+            accusantium voluptatem, nam hic eligendi? Vel consequuntur et,
+            repellendus cumque omnis praesentium nesciunt illo.
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 
