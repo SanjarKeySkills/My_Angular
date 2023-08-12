f29202e2e08c460af2d62c89dfd520efde6a2ec3
SanjarKeySkills <forest2004@mail.ru>
2023-08-12 23:37:46 +0600

    add 5th ptoduct to data|products

--- src/app/data/product.ts
+++ src/app/data/product.ts
@@ -38,7 +38,7 @@ export const products: IProduct[] = [
     }
   },
   {
-    id: 2,
+    id: 4,
     title: 'Women Casual Premium Slim Fit T-Shirts ',
     price: 22.3,
     description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
@@ -49,5 +49,16 @@ export const products: IProduct[] = [
       count: 259
     }
   },
-
+  {
+    id: 5,
+    title: 'Men Casual Premium Slim Fit T-Shirts ',
+    price: 22.3,
+    description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+    category: 'men\'s clothing',
+    image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+    rating: {
+      rate: 3.1,
+      count: 259
+    },
+  },
 ]
