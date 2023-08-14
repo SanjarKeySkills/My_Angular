7cc1b31848142f11f863dbbbced25b536810749d
SanjarKeySkills <forest2004@mail.ru>
2023-08-14 23:00:16 +0600

    add #8 prod

--- src/app/data/product.ts
+++ src/app/data/product.ts
@@ -87,4 +87,17 @@ export const products: IProduct[] = [
   },
 
 
+  {
+    id: 8,
+    title: 'Child Casual Premium Slim Fit T-Shirts ',
+    price: 28.3,
+    description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+    category: 'men\'s clothing',
+    image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+    rating: {
+      rate: 5.1,
+      count: 258
+    },
+  },
+
 ]
