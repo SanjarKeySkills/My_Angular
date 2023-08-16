04e81d52ab621db40a4868fa67bca579147941b8
SanjarKeySkills <forest2004@mail.ru>
2023-08-16 22:24:05 +0600

    deleted 6,7,8 dete product

--- src/app/data/product.ts
+++ src/app/data/product.ts
@@ -60,44 +60,5 @@ export const products: IProduct[] = [
       rate: 3.1,
       count: 259
     },
-  },
-  {
-    id: 6,
-    title: 'Child Casual Premium Slim Fit T-Shirts ',
-    price: 26.3,
-    description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
-    category: 'men\'s clothing',
-    image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
-    rating: {
-      rate: 4.1,
-      count: 253
-    },
-  },
-  {
-    id: 7,
-    title: 'Child Casual Premium Slim Fit T-Shirts ',
-    price: 28.3,
-    description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
-    category: 'men\'s clothing',
-    image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
-    rating: {
-      rate: 5.1,
-      count: 258
-    },
-  },
-
-
-  {
-    id: 8,
-    title: 'Child Casual Premium Slim Fit T-Shirts ',
-    price: 28.3,
-    description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
-    category: 'men\'s clothing',
-    image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
-    rating: {
-      rate: 5.1,
-      count: 258
-    },
-  },
-
+  }
 ]
