798011defcf85528ecef2f152a737b715f4d6d37
SanjarKeySkills <forest2004@mail.ru>
2023-08-29 00:37:27 +0600

    add #8 to data prod

--- src/app/data/product.ts
+++ src/app/data/product.ts
@@ -18,6 +18,7 @@ export const products: IProduct[] = [
     id: 2,
     title: 'Mens Casual Premium Slim Fit T-Shirts ',
     price: 22.3,
+    currency: "usd",
     description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
     category: 'men\'s clothing',
     image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
@@ -30,6 +31,7 @@ export const products: IProduct[] = [
     id: 3,
     title: 'Mens Casual Premium XXL T-Shirts ',
     price: 22.3,
+    currency: "Eu",
     description: 'XXL style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
     category: 'men\'s clothing',
     image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
@@ -42,6 +44,7 @@ export const products: IProduct[] = [
     id: 4,
     title: 'Women Casual Premium Slim Fit T-Shirts ',
     price: 22.3,
+    currency: "kgSom",
     description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
     category: 'men\'s clothing',
     image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
@@ -54,6 +57,7 @@ export const products: IProduct[] = [
     id: 5,
     title: 'Men Casual Premium Slim Fit T-Shirts ',
     price: 22.3,
+    currency: "HUkr",
     description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
     category: 'men\'s clothing',
     image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
@@ -66,6 +70,7 @@ export const products: IProduct[] = [
     id: 6,
     title: 'THIS IS COMPLETELY NEW OBJECT = Men Casual Premium Slim Fit T-Shirts ',
     price: 22.3,
+    currency: "turk lirasi",
     description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
     category: 'men\'s clothing',
     image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
@@ -78,6 +83,7 @@ export const products: IProduct[] = [
     id: 7,
     title: 'THIS IS COMPLETELY NEW OBJECT = Men Casual Premium Slim Fit T-Shirts ',
     price: 22.3,
+    currency: "Eu",
     description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
     category: 'men\'s clothing',
     image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
@@ -90,7 +96,7 @@ export const products: IProduct[] = [
     id: 8,
     title: 'THIS IS also COMPLETELY NEW OBJECT = Men Casual Premium Slim Fit T-Shirts ',
     price: 25.3,
-    // currency: "turk lirasi",
+    currency: "turk lirasi",
     description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
     category: 'men\'s clothing',
     image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
@@ -99,5 +105,19 @@ export const products: IProduct[] = [
       count: 259
     },
   },
+  {
+    id: 9,
+    title: 'THIS IS also COMPLETELY NEW OBJECT = Men Casual Premium Slim Fit T-Shirts ',
+    price: 25.3,
+    currency: "turk lirasi",
+    description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+    category: 'men\'s clothing',
+    image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+    rating: {
+      rate: 3.1,
+      count: 259
+    },
+  },
+
 
 ]
