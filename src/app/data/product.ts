d0e7d1d17a0c42612a374eeedc30ed0818521f32
SanjarKeySkills <forest2004@mail.ru>
2023-08-31 23:41:05 +0600

    del num 3 data prod

--- src/app/data/product.ts
+++ src/app/data/product.ts
@@ -1,20 +1,6 @@
 import {IProduct} from "../models/product";
 
 export const products: IProduct[] = [
-
-    {
-    id: 3,
-    title: 'Mens Casual Premium XXL T-Shirts ',
-    price: 22.3,
-    currency: "Eu",
-    description: 'XXL style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
-    category: 'men\'s clothing',
-    image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
-    rating: {
-      rate: 2.1,
-      count: 259
-    }
-  },
   {
     id: 4,
     title: 'Women Casual Premium Slim Fit T-Shirts ',
